4bd41f0fa57074ff29bbe9ea63ebb2503795765d
rueleric <rueleric@gmail.com>
2024-10-05 23:45:31 -0400

    test

--- rip to mp4.bat
+++ rip to mp4.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
 cd c:\yt\dl
-c:\yt\yt-dlp.exe -f bv+ba/b --add-metadata --merge-output-format mp4 --postprocessor-args "-c:v libx264 -c:a aac"   https://www.youtube.com/playlist?list=PL4vQCiabtH-jxGzLM8ykAOecYiudFngd7
+c:\yt\yt-dlp.exe -f bv+ba/b --add-metadata --merge-output-format mp4 --postprocessor-args "-c:v libx264 -c:a aac"    https://www.youtube.com/playlist?list=PL4vQCiabtH-jxGzLM8ykAOecYiudFngd7
